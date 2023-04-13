7bd3fc2231d38a9a020ef2dfafdb07d824b42470
onelanbin <130176903+onelanbin@users.noreply.github.com>
2023-04-13 17:58:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,35 +1,19 @@
 import pandas as pd
 import numpy as np
-import math
+from scipy.stats import norm
+from statsmodels.stats.proportion import proportions_ztest
+
 
 chat_id = 1266169265 # Ваш chat ID, не меняйте название переменной
-
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-
-    # proportion of sales in control group
-    p_ctrl = x_success / x_cnt
-    
-    # proportion of sales in test group
-    p_test = y_success / y_cnt
-    
-    # pooled standard error
-    p_pool = (x_success + y_success) / (x_cnt + y_cnt)
-    se_pool = math.sqrt(p_pool * (1 - p_pool) * (1/x_cnt + 1/y_cnt))
-    
-    # z-score
-    z = (p_test - p_ctrl) / se_pool
-    
-    # critical z-value for two-tailed test with alpha = 0.09
-    crit_val = 1.645
-    
-    # compare z-score to critical z-value
-    if z > crit_val or z < -crit_val:
-        # reject null hypothesis, i.e. the new selling method is better
-        return True
-    else:
-        # fail to reject null hypothesis
-        return False
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 0.09
+    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], value=0, alternative='smaller')
+    z_crit = np.abs(norm.ppf(alpha))
+    return z_stat > z_crit
